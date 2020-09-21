34d10814d5166a8b6633cd23bb3b893104b429c0
Akatsuki_Miao <630495033@qq.com>
2020-09-21 15:22:48 +0800

    折线图组件

---
+++ components/lineChart/lineChart.vue
@@ -0,0 +1,125 @@
+<template>
+	<view class="qiun-columns">
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt">
+			<view class="qiun-title-dot-light">{{title}}</view>
+		</view>
+		<view class="qiun-charts">
+			<canvas :canvas-id="canvasId" :id="canvasId" class="charts" @touchstart="touchLineA"></canvas>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uCharts from '@/components/u-charts/u-charts.js';
+	export default {
+		name: "lineChart",
+		data() {
+			return {
+				cWidth: '',
+				cHeight: '',
+				pixelRatio: 1,
+				_self: null,
+				canvaLineA: null,
+			}
+		},
+		props: {
+			canvasId: '',
+			title: '',
+			datas: null,
+		},
+		mounted() {
+			this._self = this;
+			this.cWidth = uni.upx2px(750);
+			this.cHeight = uni.upx2px(500);
+			this.getServerData();
+		},
+		methods: {
+			getServerData() {
+				if(this.datas == null){
+					var LineA = {
+						categories: ['9月17日', '9月18日', '9月19日', '9月21日', '9月20日', '9月21日'],
+						series: [{
+							name: '机器1',
+							data: [35, 20, 25, 37, 4, 20],
+							color: '#000000'
+						}, {
+							name: '机器2',
+							data: [70, 40, 65, 100, 44, 68]
+						}, {
+							name: '机器3',
+							data: [100, 80, 95, 31, 21, 31]
+						}]
+					};
+				}else{
+					var LineA = this.datas
+				}
+				this._self.showLineA(this.canvasId, LineA, this._self);
+			},
+			showLineA(canvasId, chartData,_self) {
+				this.canvaLineA = new uCharts({
+					$this: _self,
+					canvasId: canvasId,
+					type: 'line',
+					fontSize: 11,
+					legend: {
+						show: true
+					},
+					dataLabel: false,
+					dataPointShape: true,
+					background: '#FFFFFF',
+					pixelRatio: _self.pixelRatio,
+					categories: chartData.categories,
+					series: chartData.series,
+					animation: true,
+					xAxis: {
+						type: 'grid',
+						gridColor: '#CCCCCC',
+						gridType: 'dash',
+						dashLength: 8
+					},
+					yAxis: {
+						gridType: 'dash',
+						gridColor: '#CCCCCC',
+						dashLength: 8,
+						splitNumber: 5,
+						min: 0,
+						max: 100,
+						format: (val) => {
+							return val.toFixed(0) + '%'
+						}
+					},
+					width: _self.cWidth * _self.pixelRatio,
+					height: _self.cHeight * _self.pixelRatio,
+					extra: {
+						line: {
+							type: 'straight'
+						}
+					}
+				});
+
+			},
+			touchLineA(e) {
+				this.canvaLineA.showToolTip(e, {
+					format: function(item, category) {
+						return category + ' ' + item.name + ':' + item.data
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	/*样式的width和height一定要与定义的cWidth和cHeight相对应*/
+	.qiun-charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+</style>
