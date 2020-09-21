d084fbebf5d67ec76f6792edd42fafc57f0b612d
Akatsuki_Miao <630495033@qq.com>
2020-09-21 14:36:30 +0800

    饼图组件

---
+++ components/pie/pie.vue
@@ -0,0 +1,199 @@
+<template>
+	<view class="qiun-columns">
+		<view class="qiun-bg-white qiun-title-bar qiun-common-mt">
+			<view class="qiun-title-dot-light">{{title}}</view>
+		</view>
+		<view class="qiun-charts qiun-rows">
+			<canvas :canvas-id="canvasId" :id="canvasId" class="charts-pie" @touchstart="touchPie"></canvas>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import uCharts from '../u-charts/u-charts.js';
+
+	export default {
+		name: 'pie',
+		data() {
+			return {
+				cWidth: '',
+				cHeight: '',
+				pixelRatio: 1,
+				serverData: '',
+				piearr: [],
+				_self: null,
+				canvaPie: null
+			}
+		},
+		props: {
+			title: '',
+			canvasId: '',
+			datas: null,
+		},
+		mounted(){
+			this._self = this;
+			this.cWidth = uni.upx2px(750);
+			this.cHeight = uni.upx2px(500);
+			this.getServerData();
+		},
+		methods: {
+			getServerData() {
+				if(this.datas == null){
+					var Pie = {
+						"series": [{
+							"name": "上料",
+							"data": 50
+						}, {
+							"name": "落布",
+							"data": 30
+						}, {
+							"name": "修理中",
+							"data": 20
+						}, {
+							"name": "打印条码",
+							"data": 18
+						}, {
+							"name": "检修",
+							"data": 8
+						}]
+					};
+				}else{
+					var Pie = this.datas
+				}
+				
+				this._self.showPie(this.canvasId, Pie,this._self);
+			},
+			showPie(canvasId, chartData, _self) {
+				this.canvaPie = new uCharts({
+					$this: _self,
+					canvasId: canvasId,
+					type: 'pie',
+					fontSize: 11,
+					legend: {
+						show: true,
+						position: 'right',
+						float: 'center',
+						itemGap: 10,
+						padding: 5,
+						lineHeight: 26,
+						margin: 5,
+						borderWidth: 1
+					},
+					background: '#FFFFFF',
+					pixelRatio: _self.pixelRatio,
+					series: chartData.series,
+					animation: true,
+					width: _self.cWidth * _self.pixelRatio,
+					height: _self.cHeight * _self.pixelRatio,
+					dataLabel: true,
+					extra: {
+						pie: {
+							labelWidth: 15
+						}
+					},
+				});
+				this.piearr = this.canvaPie.opts.series;
+			},
+			touchPie(e) {
+				this.canvaPie.showToolTip(e, {
+					format: function(item) {
+						return item.name + ':' + item.data
+					}
+				});
+			},
+		}
+	}
+</script>
+
+<style>
+	page {
+		background: #F2F2F2;
+		width: 750upx;
+		overflow-x: hidden;
+	}
+
+	.qiun-padding {
+		padding: 2%;
+		width: 96%;
+	}
+
+	.qiun-wrap {
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.qiun-rows {
+		display: flex;
+		flex-direction: row !important;
+	}
+
+	.qiun-columns {
+		display: flex;
+		flex-direction: column !important;
+	}
+
+	.qiun-common-mt {
+		margin-top: 10upx;
+	}
+
+	.qiun-bg-white {
+		background: #FFFFFF;
+	}
+
+	.qiun-title-bar {
+		width: 96%;
+		padding: 10upx 2%;
+		flex-wrap: nowrap;
+	}
+
+	.qiun-title-dot-light {
+		border-left: 10upx solid #0ea391;
+		padding-left: 10upx;
+		font-size: 32upx;
+		color: #000000
+	}
+
+	.qiun-charts {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.charts-pie {
+		width: 750upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.charts-right {
+		display: flex;
+		align-items: center;
+		width: 250upx;
+		height: 500upx;
+		background-color: #FFFFFF;
+	}
+
+	.legend-itme {
+		width: 200upx;
+		margin-left: 30upx;
+		height: 50upx;
+		align-items: center;
+	}
+
+	.legend-itme-point {
+		width: 20upx;
+		height: 20upx;
+		margin: 15upx;
+		border: 1px solid #FFFFFF;
+		border-radius: 20upx;
+		background-color: #000000;
+	}
+
+	.legend-itme-text {
+		height: 50upx;
+		line-height: 50upx;
+		color: #666666;
+		font-size: 26upx;
+	}
+</style>
--- pages/Api/index.vue
+++ pages/Api/index.vue
@@ -1,18 +1,36 @@
 <template>
 	<view class="container">
-		sadasdas
-		
+		<pie title="组件测试" canvasId="asd"></pie>
+		<pie title="组件测试2" canvasId="asd2" :datas="datas"></pie>
 	</view>
 </template>
 
 <script>
+	import pie from '../../components/pie/pie.vue'
 	export default {
 		components: {
-
+			pie
 		},
 		data() {
 			return {
-				
+				datas: {
+					"series": [{
+						"name": "asd",
+						"data": 50
+					}, {
+						"name": "落qwe布",
+						"data": 30
+					}, {
+						"name": "修理qwe中",
+						"data": 20
+					}, {
+						"name": "打印条码",
+						"data": 18
+					}, {
+						"name": "检修",
+						"data": 8
+					}]
+				}
 			}
 		},
 		methods: {
