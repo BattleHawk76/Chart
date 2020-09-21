eb67c44ebbf2fe58ce9ee5c00c8dc4c63cd6ecc9
梁强强 <1141432437@qq.com>
2020-09-21 13:20:40 +0800

    稍微修改(未修改完)

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,22 @@
 <template>
 	<view class="container">
 		<div class="Machine">
-			<uni-card title="机器" thumbnail="" :extra="item.name" note="Tips" v-for="(item,index) in MachineItemList" :key="index"
+			<uni-card title="机器" thumbnail="" :extra="item.name" note="详情点击" v-for="(item,index) in MachineItemList" :key="index"
 			 @click="showDialog(item)" style="width: 45%;">
-				今日开机率 {{item.availability}}<br>
-				今日生产米数 {{item.productionMeters}}<br>
-				当前状态 {{item.status}}<br>
-				状态更新时间 {{item.statusUpdateTime}}<br>
-				<view slot="footer">详情点击</view>
+				<div>
+					今日开机率:
+					<u-tag :text="item.availability+'%'" mode="light" :type="availabilityType(item.availability)" size="mini" />
+				</div>
+				<div>
+					今日生产米数 {{item.productionMeters}}
+				</div>
+				<div>
+					当前状态 {{item.status}}
+				</div>
+				<div>
+					状态更新时间 {{item.statusUpdateTime}}
+				</div>
+				
 			</uni-card>
 		</div>
 		<u-popup v-model="DialogFlag" mode="center" border-radius="14" width="80%" class="Machine">
@@ -30,58 +39,65 @@
 			return {
 				MachineItemList: [{ //后端传来的数据.此时为模拟数据
 						name: '1',
-						availability: '30%',
-						productionMeters: '100M',
+						availability: 30,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 					{
 						name: '2',
-						availability: '20%',
-						productionMeters: '100M',
+						availability: 30,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 					{
 						name: '3',
-						availability: '50%',
-						productionMeters: '100M',
+						availability: 60,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 					{
 						name: '4',
-						availability: '60%',
-						productionMeters: '100M',
+						availability: 40,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 					{
 						name: '5',
-						availability: '80%',
-						productionMeters: '100M',
+						availability: 80,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 					{
 						name: '6',
-						availability: '100%',
-						productionMeters: '100M',
+						availability: 100,
+						productionMeters: 100,
 						status: 'fine',
 						statusUpdateTime: '11:15:32'
 					},
 				],
-				DialogFlag:false,
-				details:{
-				}
+				DialogFlag: false,
+				details: {},
+				
 			}
 		},
 		methods: {
 			showDialog(item) {
-				this.DialogFlag=true
-				this.details=item
+				this.DialogFlag = true
+				this.details = item
+			},
+			availabilityType(value){
+				if(value===0)return 'info'
+				if(value<=30)return 'error'
+				if(value<=60)return 'warning'
+				if(value>=80)return 'primary'
 			}
-		}
+		},
+
 	}
 </script>
 
